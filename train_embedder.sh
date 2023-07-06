23813b908a33743733f790942a99bf12d70718ff
lihenglin <lhlin@stanford.edu>
2023-07-05 23:59:08 -0700

    update VAE embedder training

--- train_embedder.sh
+++ train_embedder.sh
@@ -3,5 +3,12 @@
 #VAE Training example
 python train_embedder.py \
   --config configs/embedder_VAE_image.json \
-  --name Embedder_VAE_square_image\
-  --dataset datasets/square_machine_policy/square_400_paired.hdf5
+  --name Embedder_VAE_square_image \
+  --dataset datasets/square_machine_policy/square_400_paired.hdf5 & \
+
+python train_embedder.py \
+  --config configs/embedder_VAE_lowdim.json \
+  --name Embedder_VAE_square_lowdim \
+  --dataset datasets/square_machine_policy/square_400_paired.hdf5 & \
+  
+wait
